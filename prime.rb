d1b16c08f31cf7e50e38ff77f2cd6ad39698df90
Yvonne M Pham <yvonnersz@gmail.com>
2020-03-28 02:02:17 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -6,11 +6,9 @@ def prime?(integer)
   elsif integer == 2 || integer == 3
     true
   elsif (2..integer-1).any? do |num|
-    if integer % num == 0
-      false
-    else
-      true
-    end
+    integer % num == 0
+    false
+  end
   end
   end
 end
