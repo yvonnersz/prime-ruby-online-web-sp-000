02eeef2166887fb0f6ca77cdb2b5a5577c15cd17
Yvonne M Pham <yvonnersz@gmail.com>
2020-03-28 01:38:25 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+
+def prime?(integer)
+  prime_nums = (1..11).to_a
+  prime_nums.each do |nums|
+    if integer % nums == 0
+      return true
+    else
+      return false
+    end
+  end
+end
