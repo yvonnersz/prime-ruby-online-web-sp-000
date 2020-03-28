4d70e7e0185ac76a1bba48084e9c36bdcebef2f4
Yvonne M Pham <yvonnersz@gmail.com>
2020-03-28 01:58:18 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,19 @@
-def prime?(n)
-  return false if n < 2
-  return true if n == 3 || n == 2
+def prime?(integer)
+  return false if n < 2 
+  return true if n == 3 || n == 2 
     if (2...n-1).any?{|i| n % i == 0}
       false
     else
       true
     end
+end
+
+def prime?(integer)
+  if integer < 1
+    false
+  elsif integer == 2
+    true
+  elsif (2..integer-1).any? {|num| integer%num == 0}
+    false
+  end
 end
