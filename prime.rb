ee03e74f00a50805854d876313bd14aa3e2e772a
Yvonne M Pham <yvonnersz@gmail.com>
2020-03-28 02:05:03 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,9 +3,9 @@
 def prime?(integer)
   if integer <= 1
     return false
-  elsif (2..n-1).any? do |integer|
-   n % integer != 0
-
+  elsif (2..integer-1).any? do |num|
+    integer % num != 0
+    return true
    end
   end
 end
