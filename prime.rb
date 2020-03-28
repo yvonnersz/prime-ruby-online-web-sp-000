1470586d5e65ece54a373b36a036799eaf49f049
Yvonne M Pham <yvonnersz@gmail.com>
2020-03-28 01:45:19 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 # Add  code here!
 
 def prime?(integer)
-  range_numbers = (3..(integer-1)).to_a # we have to divide integer with a range of numbers
+  range_numbers = (2..(integer-1)).to_a # we have to divide integer with a range of numbers
   range_numbers.each do |num|
     if integer % num != 0
       true
