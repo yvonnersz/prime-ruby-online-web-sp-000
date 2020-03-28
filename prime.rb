2b3a62f45ce8a5e3697e092779122030e6956c1a
Yvonne M Pham <yvonnersz@gmail.com>
2020-03-28 01:43:53 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,4 +2,9 @@
 
 def prime?(integer)
   range_numbers = (2..(integer-1)).to_a # we have to divide integer with a range of numbers
+  if integer % range_numbers != 0
+    true
+  else
+    return false
+  end
 end
